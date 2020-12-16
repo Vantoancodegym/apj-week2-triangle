1514707b54715839a7d26eb91267fa9cddf83400
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2020-12-16 16:37:49 +0700

    triangle

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle("red",true,5,5,5);
+        System.out.println(triangle.toString());
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,34 @@
+public class Shape {
+    private String color;
+    private Boolean filled;
+    public Shape(){};
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Boolean getFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
